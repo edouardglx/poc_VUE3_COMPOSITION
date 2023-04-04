f46681f6bfdf242715cf02607c2f9e1fab1c2f94
edouardglx <edouard.gloux@zenika.com>
2023-04-04 17:09:41 +0200

    Created Header component
    Implemented Header component
    
    deleted console
    
    Fetched the data

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { reactive } from 'vue'
+
+const state = reactive({
+  login: false
+})
+const toto = 'hello world'
+
+const Fetchdata = async () => {
+  const response = await fetch('')
+    .then((res) => res.json())
+    .then((err) => err.message)
+  return response
+}
+</script>
+
+<template>
+  <div class="header--component">
+    <a href="#">Home</a>
+    <a href="#">Login</a>
+    <a href="#">Favoris</a>
+  </div>
+</template>
+
+<style></style>
