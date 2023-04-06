e7fe3a8654ad8eaa6485e4204a48216599e8e46a
edouardglx <edouard.gloux@zenika.com>
2023-04-06 18:27:22 +0200

    Initialised API service

--- src/components/GetAllBeers.vue
+++ src/components/GetAllBeers.vue
@@ -7,29 +7,6 @@ import SearchBar from '../components/SearchBeers.vue'
 const state = reactive({
   beersList: []
 })
-
-const findASpecificBeer = async (beerResearch) => {
-  if (beerResearch) {
-    try {
-      const response = await axios.get(
-        `https://api.punkapi.com/v2/beers/?beer_name=` + `${beerResearch}`
-      )
-      state.beersList = response.data
-    } catch (error) {
-      console.log(error.message)
-    }
-  }
-}
-
-const fetchBeer = async () => {
-  try {
-    const response = await axios.get('https://api.punkapi.com/v2/beers?page=1&per_page=50')
-    state.beersList = response.data
-  } catch (error) {
-    console.log(error.message)
-  }
-}
-fetchBeer()
 </script>
 
 <template>
