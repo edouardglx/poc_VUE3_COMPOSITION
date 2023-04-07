3c326b5f8a6259416cb0ac60d4c7c8854e84abe2
edouardglx <edouard.gloux@zenika.com>
2023-04-07 17:56:40 +0200

    Created API service

--- src/service/api.js
+++ src/service/api.js
@@ -1,23 +1,34 @@
-//SEARCH
-const findASpecificBeer = async (beerResearch) => {
+import axios from 'axios'
+
+//SEARCH_A_SPECIFIC_BEER
+export const findASpecificBeer = async (beerResearch) => {
   if (beerResearch) {
     try {
       const response = await axios.get(
         `https://api.punkapi.com/v2/beers/?beer_name=` + `${beerResearch}`
       )
-      state.beersList = response.data
+      return response.data
     } catch (error) {
-      console.log(error.message)
+      console.dir(error.message)
     }
+  }
+}
+//BEER_DETAIL
+export const fetchBeerDetail = async (id) => {
+  try {
+    const response = await axios.get(`https://api.punkapi.com/v2/beers/${id}`)
+    return response.data
+  } catch (error) {
+    console.log(error.message)
   }
 }
 
 //LIST_OF_ALL_BEERS
-const listOfAllBeer = async () => {
+export const listOfAllBeer = async () => {
   try {
     const response = await axios.get('https://api.punkapi.com/v2/beers?page=1&per_page=50')
-    state.beersList = response.data
+    return response.data
   } catch (error) {
-    console.log(error.message)
+    console.dir(error.message)
   }
 }
