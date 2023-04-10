51df0fdf5c10ae30bedfe08a0bf7a9ecba7156b8
edouardglx <edouard.gloux@zenika.com>
2023-04-10 17:03:50 +0200

    deployed process

--- router/router.js
+++ router/router.js
@@ -1,8 +1,11 @@
 import BeerPage from '../src/views/BeerPage.vue'
 import BeerDetailsPage from '../src/views/BeerDetailsPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+
+const urlenv = '/poc_VUE3_COMPOSITION/'
+
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(urlenv),
   routes: [
     {
       path: '/',
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/poc_VUE3_COMPOSITION/',
   plugins: [vue()],
   resolve: {
     alias: {
